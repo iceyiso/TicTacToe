70933d0bcec6e1141e2cb95e9c5d2a726a23603f
iceyiso <iceyiso@gmail.com>
2024-02-26 18:04:07 -0700

    Completed tutorial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
 function Square({value, onSquareClicked}) {
@@ -14,18 +13,18 @@ function Square({value, onSquareClicked}) {
   )
 }
 
-export default function Board() {
+function Board({xIsNext, squares, onPlay}) {
 
   // const [value, setValue] : value stores the value and setValue is a function that can be used to change the value
   // The value passed in useState is the initial value
-  const [xIsNext, setXIsNext] = useState(true)
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  // const [xIsNext, setXIsNext] = x
+  // const [squares, setSquares] = useState(Array(9).fill(null));
   const winner = calculateWinner(squares);
   let status;
   
   function handleClick(i) {
     //Returns if square already clicked and if theres a winner
-    if (squares[i] || calculateWinner(setSquares)) return;
+    if (squares[i] || calculateWinner(squares)) return;
     //Creates a copy to squares array as nextSquares
     const nextSquares = squares.slice();
     //Checks turn
@@ -36,8 +35,10 @@ export default function Board() {
     }
     //Lets React know the state of the component has changed
     //Updating board state and turn
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    // setSquares(nextSquares);
+    // setXIsNext(!xIsNext);
+
+    onPlay(nextSquares);
   }
 
   if (winner) {
@@ -68,6 +69,65 @@ export default function Board() {
   )
 }
 
+export default function Game() {
+  
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  //xIsNext is true when current move is even
+  //xIsNext is false when current move is odd
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(newSquares) {
+    //Updates history (appends the updates squares array as a new history entry) and toggles player turn
+    //Enumerated all the items in history
+    // setHistory([...history, newSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), newSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    //Set xIsNext to true if the number that you're changing currentMove to is even
+  }
+
+  //For each move in the game's history, created a list item <li> which contains a button <button>
+  const moves = history.map((squares, move) => {
+      let description;
+      if (move > 0) {
+        description = "Go to move #" + move;
+      } else {
+        description = "Got to game start";
+      }
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{description}</button>
+        </li>
+      );
+    }
+  );
+
+  return (
+    <div>
+      <div>
+        <h1>TIC-TAC-TOE</h1>
+      </div>
+      <div className='game'>
+        <div className='game-board'>
+          <h2>Game</h2>
+          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/> 
+        </div>
+        <div className='game-info'>
+          <h2>History</h2>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    </div>
+    
+  )
+}
+
 function calculateWinner(squares) {
   //All possible win states
   const lines = [
@@ -83,7 +143,9 @@ function calculateWinner(squares) {
 
   for (let i=0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] == squares[b] && squares[a] == squares[c]) return squares[a];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) return squares[a];
   }
   return null;
 }
+
+
